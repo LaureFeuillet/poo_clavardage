25a5a7953d3b88cb530e9930cea2762b7c279ea8
MaelPlantec <mael.plantec@gmail.com>
2018-12-19 13:01:48 +0100

    Views

--- src/PseudoView.java
+++ src/PseudoView.java
@@ -8,81 +8,12 @@ public class PseudoView extends javax.swing.JFrame {
     /* Constructors */
     public PseudoView(Controller controller) {
         this.controller = controller;
-        initComponents();
     }
-
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        jLabel1 = new javax.swing.JLabel();
-        checkButton = new javax.swing.JButton();
-        newPseudo = new javax.swing.JTextField();
-        error = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("Choose your pseudo :");
-
-        checkButton.setText("Check !");
-        checkButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                checkButtonActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(141, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(newPseudo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(150, 150, 150))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(167, 167, 167)
-                        .addComponent(checkButton))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(189, 189, 189)
-                        .addComponent(error)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(66, 66, 66)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(newPseudo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(error)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
-                .addComponent(checkButton)
-                .addGap(23, 23, 23))
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void checkButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkButtonActionPerformed
-        // Tentative de changePseudo :
-        controller.setPseudo(pseudo);
-    }//GEN-LAST:event_checkButtonActionPerformed
+    
+    // controller.setPseudo(pseudo);
     
     public void printMsgError(){
-        error.setText("Error, this pseudo is already used !");
-        newPseudo.setText("");
+        //error.setText("Error, this pseudo is already used !");
+        //newPseudo.setText("");
     }
-
-    
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton checkButton;
-    private javax.swing.JLabel error;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField newPseudo;
-    // End of variables declaration//GEN-END:variables
 }
